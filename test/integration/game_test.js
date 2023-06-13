40096d249bcd2e63e6ed221b85d1adf2735dea74
CalPin <calum.pinder@softwire.com>
2023-06-13 08:55:36 +0100

    Integration tests

--- test/integration/game_test.js
+++ test/integration/game_test.js
@@ -10,4 +10,43 @@ describe('league app', function () {
 
     expect(game.sendCommand('print')).to.equal('No players yet');
   });
+
+  it('adds a player to the game and prints the new game state', function () {
+    const game = app.startGame(gameState.createLeague());
+
+    game.sendCommand('add player Calum');
+
+    expect(game.sendCommand('print')).to.match(/^[^\w]+Calum[^\w]+$/);
+  });
+
+  it('adds a second player to the game on a new row and prints the new game state', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Calum');
+
+    game.sendCommand('add player Kenny');
+
+    expect(game.sendCommand('print')).to.match(/^[^\w]+Calum[^\w]+Kenny[^\w]+$/);
+  });
+
+  it('records a win', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Calum');
+    game.sendCommand('add player Kenny');
+
+    game.sendCommand('record win Kenny Calum');
+
+    expect(game.sendCommand('print')).to.match(/^[^\w]+Kenny[^\w]+Calum[^\w]+$/);
+  });
+
+  it('Saves and retrieves game state from a local fs', function () {
+    const game = app.startGame(gameState.createLeague());
+    game.sendCommand('add player Calum');
+    game.sendCommand('add player Kenny');
+    game.sendCommand('save test/integration/save1.json');
+
+    const newGame = app.startGame(gameState.createLeague());
+    newGame.sendCommand('load test/integration/save1.json');
+
+    expect(newGame.sendCommand('print')).to.match(/^[^\w]+Calum[^\w]+Kenny[^\w]+$/);
+  });
 });
